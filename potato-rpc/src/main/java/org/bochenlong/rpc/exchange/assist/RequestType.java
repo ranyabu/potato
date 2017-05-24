7304004a0b8e43a5daefeedceebaed70a7e70bd9
bochenlong <317144004@qq.com>
2017-05-24 17:28:06 +0800

    add

--- potato-rpc/src/main/java/org/bochenlong/rpc/exchange/assist/RequestType.java
+++ potato-rpc/src/main/java/org/bochenlong/rpc/exchange/assist/RequestType.java
@@ -4,9 +4,8 @@ package org.bochenlong.rpc.exchange.assist;
  * Created by bochenlong on 17-3-27.
  */
 public enum RequestType {
-    ASYNC((byte) 1),    // 异步请求,会收到回复
-    SYNC((byte) 2),     // 同步请求,会收到回复
-    NOTIFY((byte) 3);   // 异步通知,相当于UDP
+    SYNC((byte) 1),     // 同步请求,会收到回复
+    NOTIFY((byte) 2);   // 异步通知,相当于UDP
 
     private byte type;
 
